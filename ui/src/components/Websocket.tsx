d4f23eea658c0c75340602310f7147685c8dc65f
Dries De Peuter <dries@stenic.io>
2023-01-12 00:37:36 +0100

    feat: Add browser notifications

--- ui/src/components/Websocket.tsx
+++ ui/src/components/Websocket.tsx
@@ -7,12 +7,10 @@ import { useAuth } from "react-oidc-context";
 const Websockets = () => {
   const authToken = useAuth().user?.access_token;
 
-  const l = window.location;
   let wsUrl = window.location.origin.replace("http", "ws") + "/socket";
   if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
     wsUrl = "ws://127.0.0.1:8080/socket";
   }
-  // wsUrl = wsUrl.replace("//", `//token:${authToken}@`);
 
   const queryClient = useQueryClient();
   const { enqueueSnackbar } = useSnackbar();
@@ -26,9 +24,22 @@ const Websockets = () => {
 
   let current: any;
   useEffect(() => {
-    if (lastMessage && lastMessage != current) {
+    if (lastMessage && lastMessage !== current) {
       queryClient.refetchQueries({ exact: false, stale: true });
       enqueueSnackbar("New version received!", { variant: "info" });
+      if (
+        localStorage.getItem("setting.notification.enabled") === "granted" &&
+        document.visibilityState !== "visible"
+      ) {
+        const notification = new Notification("Ledger", {
+          body: "New version received!",
+          icon: "/icon.png",
+        });
+        notification.onclick = () => {
+          notification.close();
+          window.parent.focus();
+        };
+      }
       current = lastMessage;
     }
   }, [lastMessage]);
--- ui/src/scenes/settings/index.tsx
+++ ui/src/scenes/settings/index.tsx
@@ -1,11 +1,85 @@
-import { Box } from "@mui/material";
+import {
+  Box,
+  FormControlLabel,
+  FormGroup,
+  Switch,
+  Typography,
+} from "@mui/material";
 import Header from "../../components/Header";
+import { useEffect, useState } from "react";
+import { useSnackbar } from "notistack";
 
 const Settings = () => {
+  const { enqueueSnackbar } = useSnackbar();
+  const [notificationEnabled, setNotificationEnabled] = useState(
+    localStorage.getItem("setting.notification.enabled") === "granted"
+  );
+
+  useEffect(() => {
+    if (localStorage.getItem("setting.notification.enabled") === null) {
+      setNotificationEnabled(Notification.permission === "granted");
+    }
+  }, []);
+
+  const notificationStorage = async (enabled: boolean) => {
+    if (enabled) {
+      switch (Notification.permission) {
+        case "default":
+          await Notification.requestPermission().then((permission) => {
+            setNotificationEnabled(permission === "granted");
+          });
+          return;
+        case "denied":
+          enqueueSnackbar("Cannot enable notifications!", { variant: "error" });
+          setNotificationEnabled(false);
+          return;
+      }
+    }
+    localStorage.setItem(
+      "setting.notification.enabled",
+      enabled ? "granted" : "denield"
+    );
+  };
+
+  useEffect(() => {
+    notificationStorage(notificationEnabled);
+  }, [notificationEnabled]);
+
+  const handleNotificationSetting: (
+    event: React.SyntheticEvent,
+    checked: boolean
+  ) => void = (event, checked) => {
+    setNotificationEnabled(checked);
+  };
+
   return (
     <Box m="20px">
       <Header title="Settings" subtitle="Application settings" />
-      TODO
+
+      <Box
+        sx={{
+          display: "grid",
+          gap: "10px",
+          gridTemplateColumns: "repeat(12, 1fr)",
+          "& > div": {
+            bgcolor: "background.paper",
+            p: 2,
+          },
+        }}
+      >
+        <FormGroup
+          sx={{
+            gridColumn: "span 6",
+          }}
+        >
+          <Typography variant="h3">Notifications</Typography>
+          <FormControlLabel
+            control={<Switch checked={notificationEnabled} />}
+            label="Enable browser notifications"
+            onChange={handleNotificationSetting}
+          />
+        </FormGroup>
+      </Box>
     </Box>
   );
 };
